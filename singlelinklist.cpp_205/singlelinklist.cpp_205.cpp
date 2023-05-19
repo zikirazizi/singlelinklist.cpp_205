1a3fb23bea8ff9f686102f1376565489de06a7c3
zikirazizi <zikir.azizi.ft22@mail.umy.ic.id>
2023-05-19 17:33:02 +0700

    data current

--- singlelinklist.cpp_205/singlelinklist.cpp_205.cpp
+++ singlelinklist.cpp_205/singlelinklist.cpp_205.cpp
@@ -107,4 +107,19 @@ void searchData() {
 		system("cls");
 		return;
 	}
-	
+	else {
+		int nim;
+		cout << "Masukan NIM: ";
+		cin >> nim;
+		Node* currentNode = START;
+		while (currentNode != NULL) {
+			if (currentNode->noMhs == nim) {
+				cout << "NIM: " << currentNode->noMhs << ", Nama: " << currentNode->name << endl;
+				return;
+			}
+			currentNode = currentNode->next;
+		}
+		cout << "Data tidak detemukan" << endl;
+	}
+}
+
