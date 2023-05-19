d6177b76cedb54b34350b291b6b522a6c460516b
zikirazizi <zikir.azizi.ft22@mail.umy.ic.id>
2023-05-19 17:33:42 +0700

    data intgermain

--- singlelinklist.cpp_205/singlelinklist.cpp_205.cpp
+++ singlelinklist.cpp_205/singlelinklist.cpp_205.cpp
@@ -122,4 +122,69 @@ void searchData() {
 		cout << "Data tidak detemukan" << endl;
 	}
 }
+int main() {
+	int pilihan;
+	do
+	{
+		try
+		{
+			cout << "1. Tambah Data" << endl;
+			cout << "2. Hapus Data" << endl;
+			cout << "3. Tampilkan Data" << endl;
+			cout << "4. Cari Data" << endl;
+			cout << "5. Keluar" << endl;
+			cout << "Pilihan: ";
+			cin >> pilihan;
+			switch (pilihan)
+			{
+			case 1:
+				addNode();
+				cout << "Data berhasil ditambahkan" << endl;
+				system("pause");
+				system("cls");
+				break;
+			case 2:
+				if (listEmpty())
+				{
+					cout << "List kosong" << endl;
+					system("pause");
+					system("cls");
+					break;
+				}
+				int nim;
+				cout << "Masukan NIM: ";
+				cin >> nim;
+				if (deleteNode(nim)) {
+					cout << "nim: " << nim << "berhasil dihapus" << endl;
+					system("pause");
+					system("cls");
+				}
+				else
+					cout << "Data tidak ditemukan" << endl; \
+					break;
+			case 3:
+				traverse();
+				break;
+			case 4:
+				searchData();
+				break;
+			case 5:
+				break;
+			default:
+				cout << "Pilihan tidak ada" << endl;
+				break;
+			}
+
+		}
+		catch (exception e)
+		{
+			cout << "Terjadi kesalahan" << endl;
+		}
+	} while (pilihan != 5);
+}
+
+
+
+
+
 
