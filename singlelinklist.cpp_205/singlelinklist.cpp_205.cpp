f7bba649d356582192a0b3a7fa2137f14fb3f917
zikirazizi <zikir.azizi.ft22@mail.umy.ic.id>
2023-05-19 17:36:05 +0700

    kasus 2

--- singlelinklist.cpp_205/singlelinklist.cpp_205.cpp
+++ singlelinklist.cpp_205/singlelinklist.cpp_205.cpp
@@ -143,5 +143,24 @@ int main() {
 				system("pause");
 				system("cls");
 				break;
-			
 
+			case 2:
+				if (listEmpty())
+				{
+					cout << "List kosong" << endl;
+					system("pause");
+					system("cls");
+					break;
+				}
+				int nim;
+				cout << "Masukan NIM: ";
+				cin >> nim;
+				if (deleteNode(nim)) {
+					cout << "nim: " << nim << "berhasil dihapus" << endl;
+					system("pause");
+					system("cls");
+				}
+				else
+					cout << "Data tidak ditemukan" << endl; \
+					break;
+			
