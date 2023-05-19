e3b74c6a01f699680548141f1d8a75a8d0990e25
zikirazizi <zikir.azizi.ft22@mail.umy.ic.id>
2023-05-19 10:52:43 +0700

    Add project files.

---
+++ singlelinklist.cpp_205/singlelinklist.cpp_205.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+	int noMhs;
+	string name;
+	Node* next;
+};
+
+Node* START = NULL;
+
+void addNode() {
+	int nim;
+	string nama;
+	Node* nodeBaru = new Node;
+	cout << "Masukkan NIM: ";
+	cin >>
+
+}
